e28c03327bda732aa691894003567a7cec5ecdf5
Chris Cornutt <enygma@phpdeveloper.org>
2012-11-02 09:42:16 -0400

    updating to switch things between sites based on the hostname

--- inc.php
+++ inc.php
@@ -1,5 +1,18 @@
 <?php
-$jsonCacheFile  = 'var/cache/php-quickfix.json';
+switch($_SERVER['HTTP_HOST']){
+	case 'phpquickfix.me':
+		$siteType = 'php';
+		break;
+	case 'jsquickfix.me':
+		$siteType = 'js';
+		break;
+	case 'websecquickfix.me':
+		$siteType = 'websec';
+		break;
+	default:
+		$siteType = 'php';
+}
+$jsonCacheFile  = 'var/cache/'.$siteType.'-quickfix.json';
 //$gimmieFeed     = 'https://gimmebar.com/api/v0/public/assets/phpquickfix';
 $gimmieFeed     = 'https://gimmebar.com/api/v0/public/assets/phpquickfix/phpquickfix';
 $wgetCmd        = 'wget -O'.$jsonCacheFile.' '.$gimmieFeed;
--- index.php
+++ index.php
@@ -1,82 +1,29 @@
 <?php
 header('Content-type: text/html; charset=UTF-8');
 include_once 'inc.php';
-
-echo '<!-- '; print_r($_SERVER); echo '-->';
 ?>
 <html>
         <head>
                 <link rel="alternate" type="application/rss+xml" href="/feed" title="phpquickfix" />
-                <style>
-                        body {
-                                background-color: #EEEEEE;
-                                margin: 0px;
-                                padding: 0px;
-                        }
-                        #content {
-                                width: 550px;
-                                margin-left: auto;
-                                margin-right: auto;
-                        }
-                        #content td {
-                                font-family: verdana, arial, helvetica;
-                        }
-                        #header {
-                                text-align: center;
-                                background-color: #eb5822;
-                        }
-                        .header-title {
-                                color: #FFFFFF;
-                                font-size: 15px;
-                        }
-                        #detail {
-                                background-color: #FFFFFF;
-                                padding: 30px;
-                        }
-                        .item {
-                                margin-bottom: 18px;
-                        }
-                        .item-title {
-                                font-size: 16px;
-                                color: #000000;
-			}
-                        .item-tagged {
-                                color: #8D8D8D;
-                                font-size: 11px;
-                        }
-                        .byline {
-                                color: #8D8D8D;
-                                text-align: center;
-                                font-size: 11px;
-                        }
-                        #nav {
-                                text-align: center;
-                                font-size: 11px;
-                                padding-bottom: 7px;
-                        }
-                        #nav a {
-                                color: #000000;
-                                font-size: 11px;
-                        }
-                </style>
+                <link rel="stylesheet" type="text/css" href="/css/<?php echo $siteType; ?>.css">
         </head>
         <body>
                 <table cellpadding="5" cellspacing="0" border="0" id="content">
                 <tr>
                         <td id="header">
-                                <a href="/"><img src="/img/onfire.jpg" border="0"></a><br/>
-                                <span class="header-title">phpquickfix.me</span>
+                                <a href="/"><img src="/img/<?php echo $siteType;?>-onfire.jpg" border="0"></a><br/>
+                                <span class="header-title"><?php echo $siteType;?>quickfix.me</span>
                         </td>
                 </tr>
                 <tr><td id="nav">
                         <a href="/feed">rss feed</a>
-                        | follow <a href="http://twitter.com/phpquickfix">@phpquickfix</a> on twitter
+                        | follow <a href="http://twitter.com/<?php echo $siteType; ?>quickfix">@<?php echo $siteType;?>quickfix</a> on twitter
                         | <a href="http://gimmebar.com/loves/phpquickfix">gimmebar</a>
                 </td></tr>
                 <tr>
                         <td id="detail">
                         <?php
-                        $json = json_decode(file_get_contents('var/cache/php-quickfix.json'));
+                        $json = json_decode(file_get_contents('var/cache/'.$siteType.'-quickfix.json'));
                          foreach($json->records as $item){ ?>
                                 <div class="item">
                                         <a href="<?php echo $item->source; ?>" class="item-title"><?php echo $item->title; ?></a><br/>
@@ -88,7 +35,7 @@ echo '<!-- '; print_r($_SERVER); echo '-->';
                         ?>
                         <br/><br/>
                         <div class="byline">
-                                phpquickfix is a production of <a href="http://phpdeveloper.org">phpdeveloper.org</a><br/>
+                               <?php echo $siteType; ?>quickfix is a production of <a href="http://phpdeveloper.org">phpdeveloper.org</a><br/>
                                 <a href="mailto:info@phpdeveloper.org">info@phpdeveloper.org</a>
                         </div>
                         </td>
